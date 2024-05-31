54762451fd4df1ef704585db23a3510cdced076e
Aleksey <Aleksey@iMac-admin-2.local>
2024-05-31 15:27:46 +1000

    new

--- git_commands/urls.py
+++ git_commands/urls.py
@@ -3,10 +3,4 @@ from . import views
 
 urlpatterns = [
     path('<git_command>', views.get_git_command),
-    # path('synch/', views.git_synch),
-    # path('save/', views.git_save),
-    # path('publish/', views.git_publish),
-    # path('branches/', views.git_branches),
-    # path('bash/', views.git_bash),
-    # path('navigation/', views.git_navigation),
 ]
