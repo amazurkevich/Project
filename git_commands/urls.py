284d01e15167f620ec74fdb110ca17dbb85c1f20
Aleksey <Aleksey@iMac-admin-2.local>
2024-05-31 19:47:59 +1000

    Разобраться с git_commands/views.py

--- git_commands/urls.py
+++ git_commands/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('<str:git_command>', views.get_git_command),
-    path('<str:git_command>', views.get_git_command_by_group, name="git")
+    # path('<str:git_command>', views.get_git_command, name='git'),
 ]
