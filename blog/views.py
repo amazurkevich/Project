0b676ac8c5a74dc2167d8cd95649c876e224b3ed
Aleksey <Aleksey@iMac-admin-2.local>
2024-06-13 22:55:18 +1000

    blog template

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,61 @@ from django.shortcuts import render
 from django.http import response, HttpResponseRedirect, HttpResponseNotFound, HttpResponse
 from django.urls import reverse
 from django.template.loader import render_to_string
+from random import choice
+
+posts = [
+    {
+        'title': 'Рыбалка',
+        'description': 'Хорошо посидели',
+        'date': '21 авг 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'
+    },
+    {
+        'title': 'Париж',
+        'description': 'Незабываемое путешествие',
+        'date': '5 сент 2020',
+        'content': '''Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Commodi distinctio doloremque et fuga iste neque, pariatur quod sit veritatis voluptates?'''
+    },
+    {
+        'title': 'Финал лиги чемпионов',
+        'description': 'Реал опять выиграл ЛЧ',
+        'date': '28 мая 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, rem.'
+    },
+    {
+        'title': 'Охота на уток',
+        'description': 'Ни одна утка не пострадала',
+        'date': '7 дек 2019',
+        'content': 'Lorem ipsum dolor sit amet.'
+    },
+    {
+        'title': 'Фестиваль огурца',
+        'description': 'Суздаль ждет тебя',
+        'date': '12 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur id inventore odit recusandae!'
+    },
+    {
+        'title': 'Нашествие',
+        'description': 'Даешь рок, но в следующем году',
+        'date': '29 июль 2021',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est mollitia recusandae rem?'
+    },
+    {
+        'title': 'Новый год',
+        'description': 'Эх, еще один год пролетел',
+        'date': '31 дек 2022',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto corporis fuga ipsam laboriosam, nesciunt non quae qui ut veniam.'
+    },
+]
 
 def index(request):
-    return render(request,'blog/index.html')
+    random_posts = []
+    random_posts_len_nums = [i for i in range(0, len(posts))]
+    rand_nums = [choice(random_posts_len_nums) for i in range(3)]
+    for i in rand_nums:
+        random_posts.append(posts[i])
+    data = {
+        "posts": random_posts
+    }
+    return render(request,'blog/index.html', context=data)
